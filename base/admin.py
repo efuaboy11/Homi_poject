5b7bc9265aa16bcde544535d116be5b6359baa4d
efuaboy11 <iseghohimhene@gmail.com>
2026-03-04 14:02:46 +0100

    changes

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import*
 # Register your models here.
-admin.site.site_header = ""
+admin.site.site_header = "Homi_backend"
 admin.site.register(Order)
 admin.site.register(Cart)
