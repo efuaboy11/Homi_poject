5e13208704a997c174970de255779d1529976a36
efuaboy11 <iseghohimhene@gmail.com>
2026-03-16 14:03:22 +0100

    changes

--- base/admin.py
+++ base/admin.py
@@ -4,3 +4,10 @@ from .models import*
 admin.site.site_header = "Homi_backend"
 admin.site.register(Order)
 admin.site.register(Cart)
+admin.site.register(Client)
+admin.site.register(StoreOwners)
+admin.site.register(Couriers)
+admin.site.register(DisableAccount)
+admin.site.register(ProductCategories)
+admin.site.register(ProductCategories)
+admin.site.register(Email)
