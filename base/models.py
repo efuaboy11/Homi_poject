cd8e1c8388e8de2474f89fc27dc70b9c93c3b8c3
efuaboy11 <iseghohimhene@gmail.com>
2026-03-23 23:17:45 +0100

    changes

--- base/models.py
+++ base/models.py
@@ -173,11 +173,11 @@ class ProductCategories(models.Model):
 class Product(models.Model):
     store_owner = models.ForeignKey(StoreOwners, on_delete=models.CASCADE)
     product_id = models.CharField(max_length=100, null=True, blank=True)
-    categories = models.ManyToManyField(ProductCategories, related_name='products', blank=True)
+    categories = models.ManyToManyField(ProductCategories, related_name='products', blank=False)
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='product_images/', blank=False, null=False)
     
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
