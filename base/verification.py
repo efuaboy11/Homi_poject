6ada0165424fd3c7522e027c759042ef11c971b3
efuaboy11 <iseghohimhene@gmail.com>
2026-03-28 00:57:41 +0100

    changes

--- base/verification.py
+++ base/verification.py
@@ -1,16 +1,16 @@
-from django.contrib.auth.backends import ModelBackend
-from django.db.models import Q
-from .models import Users
+# from django.contrib.auth.backends import ModelBackend
+# from django.db.models import Q
+# from .models import Users
 
-class EmailOrPhoneBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        try:
-            user = Users.objects.get(
-                Q(email=username) | Q(phone_number=username)
-            )
-        except Users.DoesNotExist:
-            return None
+# class EmailOrPhoneBackend(ModelBackend):
+#     def authenticate(self, request, username=None, password=None, **kwargs):
+#         try:
+#             user = Users.objects.get(
+#                 Q(email=username) | Q(phone_number=username)
+#             )
+#         except Users.DoesNotExist:
+#             return None
 
-        if user.check_password(password):
-            return user
-        return None
+#         if user.check_password(password):
+#             return user
+#         return None
